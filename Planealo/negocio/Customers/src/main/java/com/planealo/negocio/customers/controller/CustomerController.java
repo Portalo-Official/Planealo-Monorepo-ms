776b22740492824e5bff22feb4f7a57a21dec143
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-17 09:37:05 +0200

    Clean imports

--- Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/controller/CustomerController.java
+++ Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/controller/CustomerController.java
@@ -3,7 +3,6 @@ package com.planealo.negocio.customers.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.planealo.negocio.customers.service.IService;
 import com.planealo.negocio.customers.service.impl.CustomerServiceImpl;
 
 @RestController
