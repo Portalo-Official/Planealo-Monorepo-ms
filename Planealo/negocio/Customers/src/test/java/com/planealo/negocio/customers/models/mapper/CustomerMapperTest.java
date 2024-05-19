69c6b9676a785a124d4327ea911ec207984fe885
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-20 01:11:46 +0200

    customer-ms: Created MapperTest

---
+++ Planealo/negocio/Customers/src/test/java/com/planealo/negocio/customers/models/mapper/CustomerMapperTest.java
@@ -0,0 +1,55 @@
+package com.planealo.negocio.customers.models.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.planealo.negocio.customers.models.dto.CustomerDTO;
+import com.planealo.negocio.customers.models.entity.Customer;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CustomerMapperTest {
+	@Autowired
+	CustomerMapper customerMaper;
+	
+	@Test
+	void entitytoDTO() {
+		
+		Customer customer = Customer.builder().id(1L).email("sample@gmail.com").nombre("peter").referencia("#1231-0F").build();
+		
+		CustomerDTO customerDTO =  this.customerMaper.usuarioToUsuarioDTO(customer);
+		
+		assertNotNull(customerDTO);
+		
+		assertEquals(customer.getNombre(), customerDTO.nombre());
+		assertEquals(customer.getEmail(), customerDTO.email());
+		assertEquals(customer.getReferencia(), customerDTO.ref());
+		assertNull(customerDTO.pass());
+	}
+	
+	@Test
+	void DTOtoEntity() {
+		
+		 CustomerDTO customerDTO = new CustomerDTO(
+		            "ref123",         // ref
+		            "Juan Perez",     // nombre
+		            "juan.perez@example.com", // email
+		            "password123"     // pass
+		        );
+		
+		Customer customer= this.customerMaper.usuarioDTOToUsuario(customerDTO);
+		
+		assertEquals( customerDTO.nombre(), customer.getNombre());
+		assertEquals( customerDTO.email(), customer.getEmail());
+		assertEquals( customerDTO.ref(), customer.getReferencia());
+		assertEquals(customer.getPassword(), customerDTO.pass());
+		assertNotNull(customer.getFechaCreacion());
+		assertNotNull(customer.getFechaUltimaActualizacion());
+		assertNotNull(customer.getUltimoInicioSesion());
+		
+	}
+}
