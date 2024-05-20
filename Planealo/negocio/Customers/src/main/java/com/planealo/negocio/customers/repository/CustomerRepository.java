fdeaa4d88a8353f303ebd6b79ef65d363b8b94c3
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-20 16:12:11 +0200

    customer-ms: Repository add Method

--- Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/repository/CustomerRepository.java
+++ Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/repository/CustomerRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.planealo.negocio.customers.models.entity.Customer;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-	Customer findByReferencia(String referencia);
-	List<Customer> findByReferenciaContaining(String referencia);
+	Optional<Customer> findByReferencia(String referencia);
+	Optional<Customer> findByEmail(String Email);
+	Optional<List<Customer>> findByReferenciaContaining(String referencia);
 }
