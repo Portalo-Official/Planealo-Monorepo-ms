82aff00a3a8b6e5e897a3d44c221623e47ac6333
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-20 22:24:12 +0200

    plan-ms: modified Plan Entity

--- Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/model/entity/Plan.java
+++ Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/model/entity/Plan.java
@@ -1,9 +1,11 @@
 package com.planealo.negocio.plan.model.entity;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,14 +13,18 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "planes")
 public class Plan {
@@ -53,8 +59,17 @@ public class Plan {
     @Column(name = "ciudad", length = 255)
     private String ciudad;
 
+    /* Lo he visto en practicas y por internet pero no se si me gusta aqui
+     * Lo dejo porque considero que el enum tiene la sohesion necesario para 
+     * depender solamente de Plan
+     */
     public enum Modo {
         PRIVADA,
-        PUBLICA
+        PUBLICA;
+    	
     }
+    
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name ="plan_id") // Probar con la tonteria de clave compuesta en PlanMemberId que esto FUNCA!!
+    private Set<PlanMember> miembros;
 }
