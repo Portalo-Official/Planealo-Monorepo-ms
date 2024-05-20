80fa3f0ee16fe437304bf7454d520fa57d3d8a54
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-20 13:17:11 +0200

    format customerDTOperfil

--- Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/models/dto/CustomerDTOperfil.java
+++ Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/models/dto/CustomerDTOperfil.java
@@ -1,3 +1,3 @@
 package com.planealo.negocio.customers.models.dto;
 
-public record CustomerDTO(String ref, String nombre, String email, String pass) {}
+public record CustomerDTOperfil(String ref, String nombre, String email, String pass) {}
