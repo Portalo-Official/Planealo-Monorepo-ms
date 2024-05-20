86abb0642d99f463827e45c52b4308a80a470933
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-20 21:58:33 +0200

    pla-ms: interface ServiceCRUD

--- Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/service/IServiceCRUD.java
+++ Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/service/IServiceCRUD.java
@@ -1,8 +1,17 @@
 package com.planealo.negocio.plan.service;
 
-public interface IService {
+import java.util.List;
 
-	
-	
-	
+public interface IServiceCRUD<T, Ref> {
+
+	T add(T entity);
+
+	boolean delete(Ref ref);
+
+	T getByRef(Ref ref);
+
+	List<T> getAll();
+
+	T update(Ref ref, T entity);
+
 }
