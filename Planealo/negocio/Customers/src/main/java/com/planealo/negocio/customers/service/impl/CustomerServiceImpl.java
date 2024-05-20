ce66134611a6647f064d75bfe06658f734b87093
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-20 22:52:47 +0200

    Clean iomports

--- Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/service/impl/CustomerServiceImpl.java
+++ Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/service/impl/CustomerServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.planealo.negocio.customers.models.entity.Customer;
 import com.planealo.negocio.customers.repository.CustomerRepository;
