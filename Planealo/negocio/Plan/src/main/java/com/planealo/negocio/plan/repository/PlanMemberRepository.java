a469ee3be556a202a8da17639ec4bb438d7998d5
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-21 12:25:06 +0200

    plan-ms: Fix error PlanMemberRepository

--- Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/repository/PlanMemberRepository.java
+++ Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/repository/PlanMemberRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.planealo.negocio.plan.model.entity.PlanMember;
 
-public interface PlanMemberRepository extends JpaRepository<PlanMember, PlanMember.PlanMiembroId>{
-	List<PlanMember>  findByUsuarioRef(String usuarioRef);
-	
+public interface PlanMemberRepository extends JpaRepository<PlanMember, PlanMember.PlanMiembroId> {
+	/*
+	 * List<PlanMember>  findByUsuarioRef(String usuarioRef); seria lo normal
+	 * como es un id compuesto hay que hacer id.usuarioRed de tal forma -> findByIdUsuarioRef
+	 */
+    List<PlanMember> findByIdUsuarioRef(String usuarioRef);
 }
