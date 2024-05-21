9324cd71d7e0b25b8b4bbb08e39007cde1127abf
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-21 12:23:34 +0200

    docker ddbb: update tables BD-Planes

--- ddbb-v1/scripts/planes/planes-initdb.sql
+++ ddbb-v1/scripts/planes/planes-initdb.sql
@@ -7,24 +7,24 @@ CREATE TABLE planes (
     ubicacion_latitud DOUBLE PRECISION,
     ubicacion_longitud DOUBLE PRECISION,
     descripcion TEXT,
-    modo VARCHAR(50) CHECK (modo IN ('privada', 'publica'))  -- Restricción para asegurarme de introducir estos valores válidos
+    modo VARCHAR(50) CHECK (modo IN ('privada', 'publica'))  -- Restricción para asegurarme de introducir estos valores
 );
 
 --  Tabla de roles
 CREATE TABLE rol (
     id BIGSERIAL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
+    nombre VARCHAR(50) NOT NULL  CHECK (modo IN ('administrador', 'invitado', 'propietario')) 
 );
 
 -- Tabla de miembros del plan
 CREATE TABLE plan_miembros (
-    plan_id BIGINT NOT NULL,
+    plan_referencia VARCHAR(150) NOT NULL,
     usuario_ref VARCHAR(150) NOT NULL,
     rol_id BIGINT NOT NULL,
-    PRIMARY KEY (plan_id, usuario_ref),
-    FOREIGN KEY (plan_id) REFERENCES planes(id),
+    PRIMARY KEY (plan_referencia, usuario_ref),
+    FOREIGN KEY (plan_referencia) REFERENCES planes(referencia),
     FOREIGN KEY (rol_id) REFERENCES rol(id)
 );
 
 -- Insertar valores en la tabla rol
-INSERT INTO rol (nombre) VALUES ('administrador'), ('invitado'), ('propietario');
+-- INSERT INTO rol (nombre) VALUES ('administrador'), ('invitado'), ('propietario');
