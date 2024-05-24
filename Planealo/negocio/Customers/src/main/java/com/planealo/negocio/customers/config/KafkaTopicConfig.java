def6bbcf37bc2b5811940f59920971764ccb697c
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-25 01:22:39 +0200

    Kafka Beam KafkaConfig basico customer-ms

---
+++ Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/config/KafkaTopicConfig.java
@@ -0,0 +1,31 @@
+package com.planealo.negocio.customers.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+	/*
+	 * Particiones: Un tópico puede estar dividido en múltiples particiones, 
+	 * 				y cada partición es un log secuencial ordenado.
+	 * 
+	 * Replicas: Copias de una partición. Cada partición en un tópico puede 
+	 * 			 tener múltiples réplicas distribuidas en diferentes brokers.
+	 * 			 Si en algun momento un topic se cae, esto sirve de resplado
+	 * 			 para que no se pierda.
+	 * 
+	 * Estos parametros sirver para manejar la carga de trabajo de manera eficiente
+	 *  y asegurar la disponibilidad de los datos.
+	 */
+	@Bean
+	public NewTopic generateTopi() {
+		
+		return TopicBuilder.name("Nombre_del_Topic")
+					.partitions(2) // Num particiones
+					.replicas(2) // Num replicas
+					.build();
+	}
+	
+}
