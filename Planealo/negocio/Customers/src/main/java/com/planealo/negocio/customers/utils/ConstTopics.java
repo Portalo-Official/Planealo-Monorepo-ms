52526fc70b90a8557b29a4a028f50feed61a7231
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-25 02:29:11 +0200

    Kafka constantes para refactorizar

---
+++ Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/utils/ConstTopics.java
@@ -0,0 +1,23 @@
+package com.planealo.negocio.customers.utils;
+
+public class ConstTopics {
+	
+	public static final String Topic_Planes     = "Planes";
+	
+	public static final String DIA_MILISEGUNDOS = "86400000";
+	
+	public static final String UN_GB            = "1073741824";
+	
+	public static final String MEDIO_GB         = "536870912";
+	
+	public static final String CUARTO_GB        = "262144000";
+	
+	public static final String UN_MB            = "1048576";
+	
+	public static final String DOS_MB           = "2097152";
+	
+	public static final String TRES_MB          = "3145728";
+	
+	public static final String CINCO_MB         = "5242880";
+	
+}
