d325fb999c090078d1a948d43866e52f3be614d3
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-26 19:00:32 +0200

    plan-ms: add feignclient decorator

--- Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/PlanApplication.java
+++ Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/PlanApplication.java
@@ -2,8 +2,9 @@ package com.planealo.negocio.plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@EnableFeignClients
 @SpringBootApplication
 public class PlanApplication {
 
