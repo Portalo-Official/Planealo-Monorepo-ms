875da038680f322f62861bc4e0d8b553bf8633b3
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-28 01:23:34 +0200

    plan-ms: create method createPlan (not implement)

--- Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/controller/PlanController.java
+++ Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/controller/PlanController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,11 +39,19 @@ public class PlanController {
 	}
 
 
-	@GetMapping("")
+	@GetMapping("all")
 	public ResponseEntity<?> getPlan(){
 		
 		List<PlanDTOresumen> planes = this.planMapper.toDTOresumenList(this.planService.getAll());
 			
+		return ResponseEntity.ok(planes);
+	}
+	
+	@PostMapping("new")
+	public ResponseEntity<?> createPlan(){
+		
+		List<PlanDTOresumen> planes = this.planMapper.toDTOresumenList(this.planService.getAll());
+		
 		return ResponseEntity.ok(planes);
 	}
 	
