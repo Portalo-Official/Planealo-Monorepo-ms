720e74712964cb4d61ee6cf0be2666164f4dbb0d
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-28 17:36:54 +0200

    plan-ms: PlanController delete imports not used

--- Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/controller/PlanController.java
+++ Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/controller/PlanController.java
@@ -1,12 +1,9 @@
 package com.planealo.negocio.plan.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,10 +15,8 @@ import com.planealo.negocio.plan.model.dto.CustomerDTOresumen;
 import com.planealo.negocio.plan.model.dto.PlanDTOaux;
 import com.planealo.negocio.plan.model.dto.PlanDTOresumen;
 import com.planealo.negocio.plan.model.entity.Plan;
-import com.planealo.negocio.plan.model.entity.PlanMember;
 import com.planealo.negocio.plan.model.mapper.PlanMapper;
 import com.planealo.negocio.plan.service.impl.PlanServiceImpl;
-import com.planealo.negocio.plan.utils.ConstTopics;
 
 @RestController
 @RequestMapping("/planes")
