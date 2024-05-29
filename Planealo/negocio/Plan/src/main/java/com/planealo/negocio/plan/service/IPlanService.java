8fd4caaf3b259ecc1e1817107fa1c1da16b64903
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-30 00:17:16 +0200

    declared method

--- Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/service/IPlanService.java
+++ Planealo/negocio/Plan/src/main/java/com/planealo/negocio/plan/service/IPlanService.java
@@ -1,7 +1,11 @@
 package com.planealo.negocio.plan.service;
 
+import java.util.List;
+
 public interface IPlanService<T, Ref> extends IServiceCRUD<T, Ref>{
 
 	T findByReferencia(Ref ref);
+	List<T> getAllByRef (List<Ref> refs);
 	T deletePlan(Ref ref);
+	
 }
