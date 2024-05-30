6fc568efab230043d17d4727bf0d3b010841d735
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-05-30 19:42:03 +0200

    customer-ms: declared login method into repository

--- Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/repository/CustomerRepository.java
+++ Planealo/negocio/Customers/src/main/java/com/planealo/negocio/customers/repository/CustomerRepository.java
@@ -1,14 +1,19 @@
 package com.planealo.negocio.customers.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.planealo.negocio.customers.models.entity.Customer;
+
+import feign.Param;
+
 import java.util.List;
 import java.util.Optional;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 	Optional<Customer> findByReferencia(String referencia);
-	Optional<Customer> findByEmail(String Email);
+	@Query("SELECT c FROM Customer c WHERE c.email = :email")
+	Optional<Customer> findByEmail(@Param("email") String email);
 	Optional<List<Customer>> findByReferenciaContaining(String referencia);
 }
